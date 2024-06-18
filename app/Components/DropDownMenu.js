b168e46c48c12b2c87b0090e7bdc55a99cc76433
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-18 21:25:18 +0530

    chnage dropdownmenu postion and styles

--- app/Components/DropDownMenu.js
+++ app/Components/DropDownMenu.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import { MdOutlineEdit, MdOutlineDeleteOutline } from 'react-icons/md';
+'use client'
+import { Icon } from '@mui/material'
+import React, { useEffect, useRef } from 'react'
+import { MdOutlineEdit,MdOutlineDeleteOutline  } from "react-icons/md";
+import { useContextProvider } from '../ContextApi';
 
-function DropDownMenu() {
+function DropDownMenu(props) {
+   const {openDropDownMenu,setOpenDropDownMenu}=useContextProvider();
+   const dropDownRef=useRef(null);
+
+    useEffect(()=>{
+      function handleOutiseClick(event){
+        if(!dropDownRef.current.contains(event.target)){
+          setOpenDropDownMenu(false);
+        }
+      }
+      document.addEventListener('click',handleOutiseClick);
+      return()=>{
+        document.removeEventListener('click',handleOutiseClick);
+      };
+
+   },[openDropDownMenu]);
   return (
-    <div className='p-4 w-48 fixed top-12 left-14 shadow-md flex rounded-lg flex-col gap-3 bg-white text-[13px]'>
+    <div
+    ref={dropDownRef}
+    className={`p-4 w-48 opacity-0 fixed top-18 right-[15rem] shadow-md ${openDropDownMenu ? 'opacity-100': ''} flex rounded-lg flex-col gap-3 bg-white text-[13px]`}>
 
-      <div className='flex flex-col items-center gap-2 p-3 border border-gray-200 rounded-md cursor-pointer group'>
+    <div className=' group flex gap-2  flex-col items-center border transition-all duration-300 hover:bg-[#5ca0f8ee] border-gray-200 rounded-md p-3 cursor-pointer '>
 
-        <MdOutlineEdit className='text-2xl text-blue-600 transition-all duration-300 group-hover:-rotate-45 group-hover:scale-110' />
-        <span className='font-semibold'>Modify</span>
+    
+        <MdOutlineEdit className='text-2xl text-blue-600 transition-all duration-300 group-hover:-rotate-45 group-hover:scale-110  '/>
+        <span className=' font-semibold'>Modify</span>
+    
+    </div>
+    
+    <div className='group flex gap-2 flex-col  transition-all duration-300 hover:bg-[#5ca0f8da] items-center border border-gray-200 rounded-md rota p-3 cursor-pointer '>
 
-      </div>
+    
+        <MdOutlineDeleteOutline className='text-2xl text-blue-600 transition-all duration-300 group-hover:rotate-45 group-hover:scale-110'/>
+        <span className=' font-semibold'>Delete</span>
+    
+    </div>
 
-      <div className='flex flex-col items-center gap-2 p-3 border border-gray-200 rounded-md cursor-pointer group rota'>
-
-        <MdOutlineDeleteOutline className='text-2xl text-blue-600 transition-all duration-300 group-hover:rotate-45 group-hover:scale-110' />
-        <span className='font-semibold'>Delete</span>
-
-      </div>
 
     </div>
-  );
+  )
 }
 
-export default DropDownMenu;
+export default DropDownMenu
